0bafffd0246d2736e6ce563fe766f514a3640cfc
OrenBochman <OrenBochman@gmail.com>
2021-10-15 13:38:08 +0300

    creating about and blog routes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,28 @@
 <script>
+    //import tailwind css
+    import "../global.css"
     import "../styles/tailwind-output.css";
-  </script>
+    //import header component from lib folder
+    import  Header from '$lib/header.svelte';
+    import  Footer from '$lib/footer.svelte'; 
+
+</script>
+
   <!-- Try some classes here -->
+
+  <Header/>
+  <main>
+      <!-- this is where the content from the route is inserted -->
+      <slot></slot>
+  </main>
+
   <h1 class="uppercase text-indigo-500">
     Hello World
   </h1>
-  
+
+
+  <h1 class="uppercase text-pink-500">
+    Referenses
+  </h1>
+
+  <Footer/> 
